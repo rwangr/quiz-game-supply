6c2dfd95a2de521cbff4ac4950f9b1a32c1d6a61
Ryan <WangR@Ryans-MacBook-Pro.local>
2018-01-15 01:17:01 +0800

    Add exception handle

--- engine.py
+++ engine.py
@@ -1,3 +1,27 @@
+'''
+MIT License
+
+Copyright (c) 2018 Ryan Wang
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
 import urllib.request,time,_thread,urllib.parse
 from PIL import Image, ImageFilter
 
@@ -14,12 +38,15 @@ class AnswerQuery:
         self.tread_stamp.append(time.time())
 
     def get_query_result(self,url):
-        headers = ('User-Agent','Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11')
         opener = urllib.request.build_opener()
-        opener.addheaders = [headers]
-        queryResult = opener.open(url).read()
+        opener.addheaders = [('User-Agent','Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19')]
+        try:
+            queryResult = opener.open(url).read()
+        except:
+            print ('Unexpected error:', sys.exc_info()[0])
+            continue
 
-        if 'baidu.com' in url:
+        if '=gbk' in url:
             queryResult=queryResult.decode('gbk').encode('utf-8').decode('utf-8')
         else:
             queryResult=str(queryResult,'utf-8')
