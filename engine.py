90826d0b5af24ff7fdc6c7ac5e2581788d009785
Ryan <WangR@Ryans-MacBook-Pro.local>
2018-01-16 00:47:20 +0800

    Format code

--- engine.py
+++ engine.py
@@ -22,62 +22,76 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
-import urllib.request,time,_thread,urllib.parse,traceback
+import urllib.request, time, _thread, urllib.parse, traceback
 from PIL import Image, ImageFilter
 
+
 class AnswerQuery:
+    def __init__(self, question, answers):
+        self.question = question['query_value']
+        self.coefficent = question['coeff']
+        self.answers = answers
+        self.sources = {}
+        self.sources[
+            'baidu'] = 'https://zhidao.baidu.com/search?lm=0&rn=10&pn=0&fr=search&ie=gbk&word=' + urllib.parse.quote(
+                self.question, encoding='gbk')
+        self.sources[
+            'sogou'] = 'http://wenwen.sogou.com/s/?w=' + urllib.parse.quote(
+                self.question) + '&ch=ww.header.ssda'
+        self.sources[
+            'sina'] = 'https://iask.sina.com.cn/search?searchWord=' + urllib.parse.quote(
+                self.question) + '&record=1'
+        self.sources[
+            'so'] = 'https://wenda.so.com/search/?q=' + urllib.parse.quote(
+                self.question)
+        self.treadStamp = []
+        self.startTime = time.time()
+        self.timeout = 6
 
-    def __init__(self,question,answers):
-        self.question=question['query_value']
-        self.coefficent=question['coeff']
-        self.answers=answers
-        self.sources={}
-        self.sources['baidu']='https://zhidao.baidu.com/search?lm=0&rn=10&pn=0&fr=search&ie=gbk&word='+urllib.parse.quote(self.question,encoding='gbk')
-        self.sources['sogou']='http://wenwen.sogou.com/s/?w='+urllib.parse.quote(self.question)+'&ch=ww.header.ssda'
-        self.sources['sina']='https://iask.sina.com.cn/search?searchWord='+urllib.parse.quote(self.question)+'&record=1'
-        self.sources['so']='https://wenda.so.com/search/?q='+urllib.parse.quote(self.question)
-        self.treadStamp=[]
-        self.startTime=time.time()
-        self.timeout=6
-
-    def query_count_thread(self,url):
+    def query_count_thread(self, url):
         self.get_answer_count(self.get_query_result(url))
         self.treadStamp.append(time.time())
 
-    def get_query_result(self,url):
+    def get_query_result(self, url):
         opener = urllib.request.build_opener()
-        opener.addheaders = [('User-Agent','Mozilla/5.0 (Linux) AppleWebKit/604.4.7 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/604.4.7')]
+        opener.addheaders = [(
+            'User-Agent',
+            'Mozilla/5.0 (Linux) AppleWebKit/604.4.7 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/604.4.7'
+        )]
 
         try:
             queryResult = opener.open(url).read()
             if '=gbk' in url:
-                queryResult=queryResult.decode('gbk').encode('utf-8').decode('utf-8')
+                queryResult = queryResult.decode('gbk').encode('utf-8').decode(
+                    'utf-8')
             else:
-                queryResult=str(queryResult,'utf-8')
+                queryResult = str(queryResult, 'utf-8')
         except Exception as e:
             _thread.exit()
 
         return queryResult
 
-    def get_answer_count(self,result):
+    def get_answer_count(self, result):
         try:
             for answer in self.answers:
-                answer['count']+=result.count(answer['value'])*self.coefficent
+                answer['count'] += result.count(
+                    answer['value']) * self.coefficent
         except Exception as e:
             _thread.exit()
 
-    def create_thread(self,url):
+    def create_thread(self, url):
         try:
-            return _thread.start_new_thread(self.query_count_thread,(url,))
+            return _thread.start_new_thread(self.query_count_thread, (url, ))
         except Exception as e:
-            print ('Unexpected error: Unable to start the thread')
+            print('Unexpected error: Unable to start the thread')
 
     def search(self):
         for source in list(self.sources.values()):
             self.create_thread(source)
 
         while True:
-            if len(self.treadStamp)==len(list(self.sources.values())) or time.time()-self.startTime>self.timeout:
+            if len(self.treadStamp) == len(list(self.sources.values(
+            ))) or time.time() - self.startTime > self.timeout:
                 break
 
         return self.answers
--- main.py
+++ main.py
@@ -22,89 +22,104 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
-import urllib.request,sys,base64,json,os,time,string,re,os,string
+import urllib.request, sys, base64, json, os, time, string, re, os, string
 from configparser import ConfigParser
 from PIL import Image
 from aip import AipOcr
 from zhon import hanzi
-from engine import AnswerQuery,AreaBlur
+from engine import AnswerQuery, AreaBlur
 
 # Parameter
-QUIZ_APPS=[
-{'app_name':'冲顶大会',
- 'answer_count':3,
- 'crop_area':(45,190,450,580), #iPhone 6,6s,7 on 1440*900 display resolution Mac
- 'mask_area':[]},
-
-{'app_name':'芝士超人',
- 'answer_count':3,
- 'crop_area':(24,120,470,500), #iPhone 6,6s,7 on 1440*900 display resolution Mac
- 'mask_area':[]},
-
-{'app_name':'头脑王者',
- 'answer_count':4,
- 'crop_area':(65,250,430,810), #iPhone 6,6s,7 on 1440*900 display resolution Mac
- 'mask_area':[(25,435,85,465),(410,435,470,465)]}, #iPhone 6,6s,7 on 1440*900 display resolution Mac
-
- {'app_name':'头脑王者(iPhone X)',
-  'answer_count':4,
-  'crop_area':(50,250,350,825), #iPhone X on 1440*900 display resolution Mac
-  'mask_area':[(20,520,70,540),(340,520,390,540)]} #iPhone X on 1440*900 display resolution Mac
+QUIZ_APPS = [
+    {
+        'app_name': '冲顶大会',
+        'answer_count': 3,
+        'crop_area': (45, 190, 450, 580),  #iPhone 6,6s,7 on 1440*900 display resolution Mac
+        'mask_area': []
+    },
+    {
+        'app_name': '芝士超人',
+        'answer_count': 3,
+        'crop_area': (24, 120, 470, 500),  #iPhone 6,6s,7 on 1440*900 display resolution Mac
+        'mask_area': []
+    },
+    {
+        'app_name': '头脑王者',
+        'answer_count': 4,
+        'crop_area': (65, 250, 430, 810),  #iPhone 6,6s,7 on 1440*900 display resolution Mac
+        'mask_area': [(25, 435, 85, 465), (410, 435, 470, 465)]
+    },  #iPhone 6,6s,7 on 1440*900 display resolution Mac
+    {
+        'app_name': '头脑王者(iPhone X)',
+        'answer_count': 4,
+        'crop_area': (50, 250, 350, 825),  #iPhone X on 1440*900 display resolution Mac
+        'mask_area': [(20, 520, 70, 540), (340, 520, 390, 540)]
+    }  #iPhone X on 1440*900 display resolution Mac
 ]
 
-NEG_KEYWORDS=['不','没']
+NEG_KEYWORDS = ['不', '没']
 
 # Configuration
 cfg = ConfigParser()
 cfg.read('secret.ini')
-client = AipOcr(cfg.get('BAIDU_OCR','APP_ID'), cfg.get('BAIDU_OCR','API_KEY'), cfg.get('BAIDU_OCR','SECRET_KEY'))
+client = AipOcr(
+    cfg.get('BAIDU_OCR', 'APP_ID'), cfg.get('BAIDU_OCR', 'API_KEY'),
+    cfg.get('BAIDU_OCR', 'SECRET_KEY'))
+
 
 def get_file_content(filePath):
     with open(filePath, 'rb') as fp:
         return fp.read()
 
-def is_contain_keywords(text,keywords):
+
+def is_contain_keywords(text, keywords):
     for word in keywords:
-        if text.count(word)>0:
+        if text.count(word) > 0:
             return True
     return False
 
-def clear_keywords(text,keywords):
+
+def clear_keywords(text, keywords):
     for word in keywords:
-        text=text.replace(word,'')
+        text = text.replace(word, '')
     return text
+
 
 # Select quiz app
 print('---------------------------------')
 print('The supported quiz app are as below:')
 for appSeq in range(len(QUIZ_APPS)):
-    print(appSeq+1, QUIZ_APPS[appSeq]['app_name'])
-selectedSeq=input(r'Press No. to select your app. Press other key to exit: ')
+    print(appSeq + 1, QUIZ_APPS[appSeq]['app_name'])
+selectedSeq = input(r'Press No. to select your app. Press other key to exit: ')
 try:
-    selectedSeq=int(selectedSeq)-1
+    selectedSeq = int(selectedSeq) - 1
     if not selectedSeq in range(len(QUIZ_APPS)):
         raise ValueError()
 except:
     exit()
-print('[',QUIZ_APPS[selectedSeq]['app_name'],']','has been selected')
+print('[', QUIZ_APPS[selectedSeq]['app_name'], ']', 'has been selected')
 
 while True:
-    nextStep=input('Press [Enter] to supply answer. Press other key to exit: ')
-    if nextStep=='':
+    nextStep = input(
+        'Press [Enter] to supply answer. Press other key to exit: ')
+    if nextStep == '':
         startTime = time.time()
 
         # Capture, crop image, and mask noise
-        screeshotPath=r'./screenshot'
+        screeshotPath = r'./screenshot'
         if not os.path.exists(screeshotPath):
             os.makedirs(screeshotPath)
-        fullCapFileName=screeshotPath+'/full_'+str(int(startTime))+'.png'
-        subjectCapFileName=screeshotPath+'/subject_'+str(int(startTime))+'.png'
-        os.system('screencapture '+fullCapFileName)
-        fullCap=Image.open(fullCapFileName)
-        if len(QUIZ_APPS[selectedSeq]['mask_area'])>0:
+        fullCapFileName = screeshotPath + '/full_' + str(
+            int(startTime)) + '.png'
+        subjectCapFileName = screeshotPath + '/subject_' + str(
+            int(startTime)) + '.png'
+        os.system('screencapture ' + fullCapFileName)
+        fullCap = Image.open(fullCapFileName)
+        if len(QUIZ_APPS[selectedSeq]['mask_area']) > 0:
             for area in QUIZ_APPS[selectedSeq]['mask_area']:
-                fullCap=fullCap.filter(AreaBlur(bounds=area))
-        fullCap.crop(QUIZ_APPS[selectedSeq]['crop_area']).save(subjectCapFileName)
+                fullCap = fullCap.filter(AreaBlur(bounds=area))
+        fullCap.crop(
+            QUIZ_APPS[selectedSeq]['crop_area']).save(subjectCapFileName)
 
         # Read cropped image
         image = get_file_content(subjectCapFileName)
@@ -112,37 +127,51 @@ while True:
         try:
             response = client.basicGeneral(image)
         except Exception as e:
-            print ('Unexpected error happened when calling Baidu OCR')
+            print('Unexpected error happened when calling Baidu OCR')
             exit()
 
-        wordsResult=response['words_result']
+        wordsResult = response['words_result']
 
         # Store recognized text in dict
-        answerCount=QUIZ_APPS[selectedSeq]['answer_count']
-        subject={}
-        subject['question']={'value':''.join([seg['words'] for seg in wordsResult[0:len(wordsResult)-answerCount]])}
-        if is_contain_keywords(subject['question']['value'],NEG_KEYWORDS):
-            subject['question']['coeff']=-1
-            subject['question']['query_value']=clear_keywords(subject['question']['value'],NEG_KEYWORDS)
+        answerCount = QUIZ_APPS[selectedSeq]['answer_count']
+        subject = {}
+        subject['question'] = {
+            'value':
+            ''.join([
+                seg['words']
+                for seg in wordsResult[0:len(wordsResult) - answerCount]
+            ])
+        }
+        if is_contain_keywords(subject['question']['value'], NEG_KEYWORDS):
+            subject['question']['coeff'] = -1
+            subject['question']['query_value'] = clear_keywords(
+                subject['question']['value'], NEG_KEYWORDS)
         else:
-            subject['question']['coeff']=1
-            subject['question']['query_value']=subject['question']['value']
-        subject['answers']=[{'value':seg['words'].strip(string.punctuation).strip(hanzi.punctuation),'count':0} for seg in wordsResult[len(wordsResult)-answerCount:]]
+            subject['question']['coeff'] = 1
+            subject['question']['query_value'] = subject['question']['value']
+        subject['answers'] = [{
+            'value':
+            seg['words'].strip(string.punctuation).strip(hanzi.punctuation),
+            'count':
+            0
+        } for seg in wordsResult[len(wordsResult) - answerCount:]]
 
         # Query question and obtain each of answer frequency
         try:
-            query=AnswerQuery(subject['question'],subject['answers'])
-            subject['answers']=query.search()
+            query = AnswerQuery(subject['question'], subject['answers'])
+            subject['answers'] = query.search()
         except Exception as e:
-            print ('Unexpected error happened when querying answer')
+            print('Unexpected error happened when querying answer')
             exit()
 
         # Print the result
         print('---------------------------------')
         print(subject['question']['value'])
         for answer in subject['answers']:
-            print (('>>' if answer['count']== max([answer['count'] for answer in subject['answers']]) else '  '),answer['value'],'[',answer['count'],']')
+            print(('>>' if answer['count'] == max(
+                [answer['count'] for answer in subject['answers']]) else '  '),
+                  answer['value'], '[', answer['count'], ']')
         print('---------------------------------')
-        print('time cost: '+str(time.time()-startTime)+'s')
+        print('time cost: ' + str(time.time() - startTime) + 's')
     else:
         exit()
